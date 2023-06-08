b48b2ddb7625afd9338fed65b3e27c99fca0f212
Juan Valer - GNOSS <36291900+juanvaler-gnoss@users.noreply.github.com>
2023-06-08 17:33:21 +0200

    Update program.php

--- program.php
+++ program.php
@@ -4,9 +4,11 @@ $folder = (empty(getenv('REMOTE_ADDR'))) ? 'output' : getenv('REMOTE_ADDR');
 
 $url = readline("Introduce la url a descargar: ");
 $name = substr($url, strrpos($url, '/') + 1);
+echo "\nCurrentDirectory: " . getcwd();
 echo "\nurl: ". $url;
 echo "\nname: ". $name;
 echo "\ncommand: curl -o ". $folder. '/'. $name. " ". $url;
+mkdir($folder);
 $output = shell_exec("curl -o " . $folder . "/" . $name . " " . $url);
 echo "$output";
 echo "\nDownloaded!\n";
